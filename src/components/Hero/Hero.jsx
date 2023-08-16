25ae260168b9882c3b0525493bd286b533a15d4c
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-16 22:30:46 +0200

    [ADD] desktop navbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,41 @@
 import "./Hero.scss";
 import vector_bottom_left from "../../assets/images/vector_1.png";
 import vector_top_right from "../../assets/images/vector_2.png";
+import github from "../../assets/images/github.png";
+import linkedin from "../../assets/images/linkedin.png";
 
 const Hero = () => {
   return (
     <div className="hero-wrapper">
       <nav>
-        <li>Home</li> <li>About</li> <li>Projects</li> <li>Contact</li>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Projects</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
       </nav>
+      <div className="hero-social-media">
+        <a href="https://github.com/CroAnna" target="_blank" rel="noreferrer">
+          <img src={github} alt="github-logo" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ana-%C5%A1karica-89805120b/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="linkedin-logo" />
+        </a>
+      </div>
       <div className="hero-vectors">
         <img className="vector-top" src={vector_top_right} alt="" />
+
         <img className="vector-left" src={vector_bottom_left} alt="" />
       </div>
       <div className="hero-content">
