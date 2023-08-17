d2f399332c6f4bbe175a3792b0293865e6ee93ef
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-17 09:49:04 +0200

    [ADD] info card desktop version, responsive

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,23 +1,50 @@
 import "./InfoCard.scss";
 import celijeDesktop from "../../assets/images/celije-desktop.png";
+import celijeMobile from "../../assets/images/celije-mobile.png";
+import airplane from "../../assets/images/airplane.png";
+
+import { useEffect, useState } from "react";
 
 const InfoCard = () => {
+  const [isTablet, setTablet] = useState(window.innerWidth >= 580);
+  const updateMedia = () => {
+    setTablet(window.innerWidth >= 580);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
   return (
     <div className="infocard-wrapper">
-      <div className="infocard-text">
-        <h3>About me</h3>
-        <h4>Passionate developer based in Karlovac, Croatia 📍</h4>
-        <p>
-          I am a Junior Developer well-versed in <b>React</b>, <b>Angular</b>,
-          <b>Node.js</b>, <b>Flutter</b>, and <b>C#</b>. My knack for creating
-          user-friendly interfaces using React and Angular, coupled with Node.js
-          backend skills, enables me to build seamless web interactions. With
-          Flutter, I craft cross-platform mobile apps efficiently. I thrive in
-          collaborative environments, valuing <b>teamwork</b> and effective
-          communication. And love to <b>travel</b> and discover new cultures!
-        </p>
+      <div className="infocard-inner">
+        <div className="infocard-text">
+          <h3>About me</h3>
+          <h4>Passionate developer based in Karlovac, Croatia 📍</h4>
+          <p>
+            I am a Junior Developer well-versed in <b>React</b>, <b>Angular</b>,
+            <b>Node.js</b>, <b>Flutter</b>, and <b>C#</b>. My knack for creating
+            user-friendly interfaces using React and Angular, coupled with
+            Node.js backend skills, enables me to build seamless web
+            interactions. With Flutter, I craft cross-platform mobile apps
+            efficiently.
+          </p>
+          <p>
+            I thrive in collaborative environments, valuing <b>teamwork</b> and
+            effective communication.
+          </p>
+          <p>
+            And love to <b>travel</b> and discover new cultures!
+          </p>
+          <div className="img-container">
+            <img src={airplane} alt="airplane" />
+          </div>
+        </div>
+        <img
+          src={isTablet ? celijeDesktop : celijeMobile}
+          alt="my skills images"
+        />
       </div>
-      <img src={celijeDesktop} alt="my skills images" />
     </div>
   );
 };
