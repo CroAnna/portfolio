25578ab4d63103ef84664f899b022fae425942e6
Ana Skarica <ana.skarica.as@gmail.com>
2023-09-04 17:36:09 +0200

    [ADD] new technologies images

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -22,12 +22,12 @@ const InfoCard = () => {
           <h3>About me</h3>
           <h4>Passionate developer based in Karlovac, Croatia 📍</h4>
           <p>
-            I am a Junior Developer well-versed in <b>React</b>, <b>Angular</b>,
+            I am a Junior Developer well-versed in <b>React</b>, <b>Vue</b>,
             {"  "}
-            <b>Node.js</b>, <b>Flutter</b>, and <b>C#</b>. My knack for creating
-            user-friendly interfaces using React and Angular, coupled with
-            Node.js backend skills, enables me to build seamless web
-            interactions. With Flutter, I craft cross-platform mobile apps
+            <b>Laravel</b>, <b>Node.js</b>, <b>Flutter</b>, and <b>C#</b>. My
+            knack for creating user-friendly interfaces using React and Angular,
+            coupled with Node.js backend skills, enables me to build seamless
+            web interactions. With Flutter, I craft cross-platform mobile apps
             efficiently.
           </p>
           <p>
