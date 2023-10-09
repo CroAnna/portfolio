ad836f6bda1ef2d172dc4800f762276827b5aac9
Ana Skarica <ana.skarica.as@gmail.com>
2023-10-09 17:28:22 +0200

    [ADD] MoveMent app

--- src/Data/ProjectList.js
+++ src/Data/ProjectList.js
@@ -28,6 +28,20 @@ export const projectList = [
     demoType: "web",
   },
   {
+    title: "MoveMent",
+    description:
+      "Winning project at the Ri-Hack hackathon: full-stack web app. Frontend dev and UI designer in our team. Users can add and find physical jobs and outdoor activities, filter them by radius using real-time location.",
+    technologies: [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+      "https://img1.daumcdn.net/thumb/R750x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbjSRIf%2FbtrtqOKnLcv%2F6fhZzWLPIJdsoQFWzOyCr1%2Fimg.jpg",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Sqlite-square-icon.svg/2048px-Sqlite-square-icon.svg.png",
+    ],
+    urlDemo: "https://youtu.be/Pd5PKW4IKP8",
+    urlGithub: "https://github.com/CroAnna/MoveMent",
+    img: "https://github.com/CroAnna/portfolio/blob/main/src/assets/images/movement.jpg?raw=true",
+    demoType: "video",
+  },
+  {
     title: "BikerZone",
     description:
       "Social media app for bikers that allows users to create and join rides, find new biker friends and see where their friends are going. ",
